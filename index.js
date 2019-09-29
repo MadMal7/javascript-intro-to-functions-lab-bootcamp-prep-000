90b6433eb0fc7ddac62473eafe6ee5c9396df07d
Malachi Scott Prah <malachiprah@gmail.com>
2019-09-29 20:56:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  if (console.log() === string.toUpperCase())
+  console.log("")
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toUpperCase()) {
+    console.log("YES INDEED!")
+    } else if (string === string.toLowerCase()) {
+      console.log("I can't hear you!")
+    } else if (string === "I love you Grandma.") {
+      console.log("I love you, too.")
+    } else (console.log("Listen here u 'lil shit..."))
+  
+  
+}
